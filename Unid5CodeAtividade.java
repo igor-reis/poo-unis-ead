11e2dd7fd39526b271bfe41d35764087cc8af063
Igor Reis <39283813+igor-reis@users.noreply.github.com>
2018-06-06 23:59:51 -0300

    Create Unid5CodeAtividade.java

---
+++ Unid5CodeAtividade.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unid5codeatividade;
+
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat; 
+
+/**
+ *
+ * @author igoor
+ */
+public class Unid5CodeAtividade {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float peso, alturaM, alturaCM, IMC;
+        String leitura;
+        
+        JOptionPane.showMessageDialog(null, "Calcular índice de massa corporal (IMC)");
+        
+        //leitura peso
+        leitura = JOptionPane.showInputDialog("Digite o seu peso em KG: ");
+        peso = Float.parseFloat(leitura);
+        
+        //leitura altura
+        leitura = JOptionPane.showInputDialog("Digite sua altura em CM: ");
+        alturaCM = Float.parseFloat(leitura);
+        
+        //converter alturaCM em alturaM
+        alturaM = alturaCM/100;
+        
+        //calculo IMC
+        IMC = peso/(alturaM*alturaM);
+        DecimalFormat df = new DecimalFormat("0.00"); //limitar em duas casas decimais
+ 
+        //classificações grau de obesidade
+        if(IMC < 16){
+            JOptionPane.showMessageDialog(null, "Seu IMC é: " + df.format(IMC) + ". Sua classificação: Magreza grave !");
+        }else if((IMC >= 16) && (IMC < 17)){
+            JOptionPane.showMessageDialog(null, "Seu IMC é: " + df.format(IMC) + ". Sua classificação: Magreza moderada !");
+        }else if((IMC >= 17) && (IMC < 18.5)){
+            JOptionPane.showMessageDialog(null, "Seu IMC é: " + df.format(IMC) + ". Sua classificação: Magreza leve !");
+        }else if((IMC >= 18.5) && (IMC < 25)){
+            JOptionPane.showMessageDialog(null, "Seu IMC é: " + df.format(IMC) + ". Sua classificação: Saudável !");
+        }else if((IMC >= 25) && (IMC < 30)){
+            JOptionPane.showMessageDialog(null, "Seu IMC é: " + df.format(IMC) + ". Sua classificação: Sobrepeso !");
+        }else if((IMC >= 30) && (IMC < 35)){
+            JOptionPane.showMessageDialog(null, "Seu IMC é: " + df.format(IMC) + ". Sua classificação: Obesidade Grau I !");
+        }else if((IMC >= 35) && (IMC < 40)){
+            JOptionPane.showMessageDialog(null, "Seu IMC é: " + df.format(IMC) + ". Sua classificação: Obesidade Grau II (severa) !");
+        }else if(IMC > 40){
+            JOptionPane.showMessageDialog(null, "Seu IMC é: " + df.format(IMC) + ". Sua classificação: Obesidade Grau III (mórbida) !");
+        }
+        
+    }
+    
+}
