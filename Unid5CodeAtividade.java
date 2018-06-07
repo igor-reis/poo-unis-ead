ecfebf26989a573e82e5c9d22f879e311e25661d
Igor Reis <39283813+igor-reis@users.noreply.github.com>
2018-06-06 23:36:29 -0300

    Update Unid5CodeAtividade.java

--- Unid5CodeAtividade.java
+++ Unid5CodeAtividade.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package unid5codeatividade;
 
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author igoor
+ * @author igor
  */
 public class Unid5CodeAtividade {
 
